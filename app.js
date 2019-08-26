97cf35dee9c265424c7d90fb557255f714602439
AlejandraCeniceros <dcac@outlook.es>
2019-08-26 10:00:01 -0700

    26 0819

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const buttons = document.querySelectorAll('button');
 
-const func = () => {
-    console.log(event)
-    alert ("Era OOP");
-
+const playnote = event => {
+    console.log(event.target.dataset.note)
 }
 
+
 buttons.forEach(
-    button => button.addEventListener("click")
+    button => button.addEventListener("click", playnote)
 );
